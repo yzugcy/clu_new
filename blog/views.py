1beeb03770430f0bacc18b11eeb1507ede2d559d
sajib1066 <sajib1066@gmail.com>
2024-03-13 21:57:14 +0800

    add blog and course pages

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+
+class BlogListView(ListView):
+    """ Blog view """
+    template_name = 'blog/blog.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template_name, context)
