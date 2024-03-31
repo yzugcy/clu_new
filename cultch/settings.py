10cab0d3b3db2086da9a9e3934836caefa58c0ff
sajib1066 <sajib1066@gmail.com>
2024-03-31 20:13:49 +0800

    create timeline apps

--- cultch/settings.py
+++ cultch/settings.py
@@ -64,6 +64,7 @@ LOCAL_APPS = [
     'pages.apps.PagesConfig',
     'blog.apps.BlogConfig',
     'virtualtour.apps.VirtualtourConfig',
+    'timeline.apps.TimelineConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
